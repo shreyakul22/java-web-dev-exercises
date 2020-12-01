f6a33c3ca6ace7df242bf82faa23d24a595e83ab
Shreya <contact.shreya22@gmail.com>
2020-11-30 18:49:10 -0600

    completed studio with bonus

---
+++ src/org/launchcode/java/studios/countchar/CountingChar.java
@@ -0,0 +1,31 @@
+package org.launchcode.java.studios.countchar;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class CountingChar {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter new word: ");
+        String word = input.nextLine();
+        char[] stringChars = word.toCharArray();
+        HashMap<Character, Integer> charCountsMap = new HashMap<>();
+        for (char oneLetter: stringChars) {
+            if (charCountsMap.containsKey(oneLetter)){
+                Integer count = charCountsMap.get(oneLetter) + 1;
+                charCountsMap.put(oneLetter, count);
+            } else if (Character.isLetter(oneLetter)) {
+                charCountsMap.put(oneLetter, 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry: charCountsMap.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue());
+        }
+
+    }
+
+
+
+}
